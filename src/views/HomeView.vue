c4c9002d175ddca445ee887add2213d261e7376a
GREEN gph <green311_@outlook.com>
2025-01-12 22:56:37 +0700

    push

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1 class="bg-success text-center">ภัทราวดี สังข์สม</h1>
+    <div class="row g-2 p-2">
+      <div class="col-6">
+        <img src="/image/1.jpg" class="img-fluid" />
+      </div>
+      <div class="col-6">
+        <img src="/image/2.jpg" class="img-fluid" />
+      </div>
+      <div class="col-6">
+        <img src="/image/3.jpg" class="img-fluid" />
+      </div>
+      <div class="col-6">
+        <img src="/image/4.jpg" class="img-fluid" />
+      </div>
+      <div class="col-12">
+        <img src="/image/5.jpg" class="img-fluid" />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
